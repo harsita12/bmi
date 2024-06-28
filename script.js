3bc46dbe036bf2c45d0c88ccb0c1d21b1db1aec5
harsita <tanuharsita@gmail.com>
2024-06-28 16:21:15 +0530

    a bug has been fixed

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ form.addEventListener('submit', function (e) {
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
     //show the result
-    results.innerHTML = <span>${bmi}</span>;
+    results.innerHTML = `<span>${bmi}</span>`;
   }
 });
